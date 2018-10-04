4957b5fa147dd461c652d82b61ecaa149971a480
George Bourazanas <george@bourazanas.com>
2018-10-04 11:40:42 +0100

    Rename App.css to App.scss and remove ajax libs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import 'normalize.css';
-import './App.css';
+import './App.scss';
 
 class App extends React.Component {
   render () {
     return (
-      <div className='App'>
+      <div className="App">
         <h1> Hello, World!</h1>
       </div>
     );
